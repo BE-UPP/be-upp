4ca21834640d47af700878c1618f3300a7405dba
William Sena Silva <william.sena@totvs.com.br>
2021-12-04 08:52:57 -0300

    Adicionado controle de sessão via auth armazenado em local storage
    Co-authored-by: Marcos Siolin Martins <siolinm@usp.br>
    Co-authored-by: Willian Hiroshi Takihi <willhiroshi@hotmail.com>
    Signed-off-by: William Sena Silva <william.sena@totvs.com.br>

--- frontend/src/auth/auth.jsx
+++ frontend/src/auth/auth.jsx
@@ -1,7 +1,7 @@
 const DOCTOR_SESSION = "doctor-session";
 const isAuthenticated = () => localStorage.getItem(DOCTOR_SESSION) !== null;
 const getToken = () => JSON.parse(localStorage.getItem(DOCTOR_SESSION));
-const login = session => {
+const login = (session) => {
   localStorage.setItem(DOCTOR_SESSION, JSON.stringify(session));
 };
 const logout = () => {
@@ -13,4 +13,4 @@ export default {
   getToken: getToken,
   login: login,
   logout: logout,
-}
+};
